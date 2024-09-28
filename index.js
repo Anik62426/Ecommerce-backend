1702885bcac499c993e61e141e7227790e5571f7
Anik62426 <aniketkumaratl@gmail.com>
2024-09-28 08:56:28 +0530

    rollback

--- index.js
+++ index.js
@@ -7,22 +7,9 @@ import multer from "multer";
 import path from "path";
 import dotenv from "dotenv"
 const app = express();
-
-import  { Users } from "../backend/models/user.model.js";
-import { Product } from "./models/product.model.js";
 // import { uploadOnCloudinary } from "./untils/cloudinary.js";
-import compression from "compression";
-
-
-app.use(compression());
-
 app.use(express.json());
 app.use(cors());
-
-// const router = require("./routes/blog");
-
-
-// app.use("/user",cartrouter)
 
 dotenv.config({
   path: './env'
@@ -58,7 +45,6 @@ app.use("/images", express.static("upload/images"));
 
 
 
-
 app.post("/upload",upload.single('product'),async(req,res)=>{
     res.json({
         success:1,
@@ -68,44 +54,44 @@ app.post("/upload",upload.single('product'),async(req,res)=>{
 
 // product schema
 
-// export const Product = mongoose.model("Product", {
-//   id: {
-//     type: Number,
-//     required: true,
-//   },
-//   name: {
-//     type: String,
-//     require: true,
-//   },
-//   image: {
-//     type: String,
-//     require: true,
-//   },
-//   category: {
-//     type: String,
-//     require: true,
-//   },
-//   new_price: {
-//     type: Number,
-//     require: true,
-//   },
-//   old_price: {
-//     type: Number,
-//     require: true,
-//   },
-//   date: {
-//     type: Date,
-//     default: Date.now,
-//   },
-//   avilable: {
-//     type: Boolean,
-//     default: true,
-//   },
-//   rating:{
-//     type: Number,
-//     require: true,
-//   },
-// });
+const Product = mongoose.model("Product", {
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    require: true,
+  },
+  image: {
+    type: String,
+    require: true,
+  },
+  category: {
+    type: String,
+    require: true,
+  },
+  new_price: {
+    type: Number,
+    require: true,
+  },
+  old_price: {
+    type: Number,
+    require: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  avilable: {
+    type: Boolean,
+    default: true,
+  },
+  rating:{
+    type: Number,
+    require: true,
+  },
+});
 
 
 app.post("/addproduct", async (req, res) => {
@@ -135,27 +121,25 @@ app.post("/addproduct", async (req, res) => {
   });
 });
 
-//  export const Users = mongoose.model('Users',{
-//   name:{
-//     type:String,
-//   },
-//   email:{
-//     type:String,
-//     unique:true,
-//   },
-//   password:{
-//     type:String,
-//   },
-//   cartData:{
-//     type:Object,
-//   },
-//   date:{
-//     type:Date,
-//     default:Date.now,
-//   }
-// })
-
-
+const Users = mongoose.model('Users',{
+  name:{
+    type:String,
+  },
+  email:{
+    type:String,
+    unique:true,
+  },
+  password:{
+    type:String,
+  },
+  cartData:{
+    type:Object,
+  },
+  date:{
+    type:Date,
+    default:Date.now,
+  }
+})
 
 app.post("/Signup",async(req,res)=>{
   let check = await Users.findOne({email:req.body.email})
@@ -277,6 +261,9 @@ app.post('/addtocart',fetchuser,async(req,res)=>{
   await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData})
   res.send("Added")
 })
+
+
+
 app.post('/removefromcart',fetchuser,async(req,res)=>{
   console.log("Removed",req.body.itemId);
   let userData = await Users.findOne({_id:req.user.id});
